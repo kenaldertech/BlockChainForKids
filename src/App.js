9968e1d3dd18008665f178785571cafb2d653994
alvarengakenneth <alvarengakenneth@gmail.com>
2024-04-28 23:07:43 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   return (
+    /*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -18,7 +19,12 @@ function App() {
           Learn React
         </a>
       </header>
+      
     </div>
+    */
+
+    <h1>Hola</h1>
+
   );
 }
 
